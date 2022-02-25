63f1dc650013ceb779eed1b658b2c5231fc80369
prem sanjeev <premsanjeev8@gmail.com>
2022-02-25 15:14:25 +0530

    Day-1

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { UserComponent } from './user.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [{ path: '', component: UserComponent,children:[{path:'user-dashboard',component:UserDashboardComponent},
+{path:'order',component:OrderComponent},
+{path:'cart',component:CartComponent}] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { OrderComponent } from './order/order.component';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    NavbarComponent,
+    FooterComponent,
+    UserDashboardComponent,
+    OrderComponent,
+    CartComponent
   ],
   imports: [
     CommonModule,
